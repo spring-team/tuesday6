cf909140470072ef4f4cea0015b99582ec88ee05
Rod Johnson <johnsonroda@gmail.com>
2018-03-06 13:41:11 +0000

    Initial commit from Atomist

---
+++ src/main/java/atomist/Tuesday6Application.java
@@ -0,0 +1,12 @@
+package atomist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tuesday6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tuesday6Application.class, args);
+	}
+}
